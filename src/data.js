4ba49d3ea77732f182e416dd64ccff33d925ffff
samymahmoud3 <samymahmoud2532001@gmail.com>
2024-01-08 02:37:25 +0200

    modified

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const choosingData = [
+    {
+      icon: 'no_fees-icon.svg',
+      iconName: 'No Fees',
+      title: "لا يوجد تكاليف اضافية",
+      description: "نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات ",
+    },
+    {
+      icon: 'calender-icon.svg',
+      iconName: 'Dates',
+      title: "التزام بالمواعيد",
+      description: "نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات ",
+    },
+    {
+      icon: 'right-icon.svg',
+      iconName: 'Easily',
+      title: "سهولة",
+      description: "نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات ",
+    },
+    {
+      icon: 'day-icon.svg',
+      iconName: 'Cancellation',
+      title: "الغاء مجانى",
+      description: "نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات ",
+    },
+]
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,47 +1,29 @@
 import FeatureItem from "../../components/featureItem/FeatureItem";
 import Header from "../../components/heroSection/HeroSection";
-import './home.scss'
+import { choosingData } from "../../data";
+import './home.scss';
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
+
       <div className="features">
         <h2 className="title">لماذا تختارنا</h2>
         <div className="items">
-          <FeatureItem className="item"
-            icon="no_fees-icon.svg"
-            iconName="No extra fees"
-            title="لا يوجد تكاليف اضافية"
-            description="نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات "
-            width={ "279px" }
-            color="#8D9094"
-          />
-          <FeatureItem className="item"
-            icon="calender-icon.svg"
-            iconName="Punctuality"
-            title="التزام بالمواعيد"
-            description="نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات "
-            width={ "279px" }
-            color="#8D9094" />
-          <FeatureItem className="item"
-            icon="right-icon.svg"
-            iconName="easily"
-            title="سهوله"
-            description="نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات "
-            width={ "279px" }
-            color="#8D9094"
-          />
-          <FeatureItem className="item"
-            icon="day-icon.svg"
-            iconName="free cancellation"
-            title="الغاء مجانى"
-            description="نهتم بتقديم افضل خدمة من حيث المستوى والخبرة في تقديم الخدمات "
-            width={ "279px" }
-            color="#8D9094"
-          />
+          { choosingData.map((item, index) => (
+            <FeatureItem className="item" key={ index }
+              icon={ item.icon }
+              iconName={ item.iconName }
+              title={ item.title }
+              description={ item.description }
+              width={ "279px" }
+              color="#8D9094"
+            />
+          )) }
         </div>
       </div>
+
     </div>
   )
 };
