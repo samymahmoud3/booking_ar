a0b3e4fe890f4d1fa204460ff3008ef03e2858ab
samymahmoud3 <samymahmoud2532001@gmail.com>
2024-01-19 23:33:33 +0200

    responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,41 @@
 import { Link, NavLink } from "react-router-dom";
 import "./navbar.scss"
 import { HashLink} from "react-router-hash-link/dist/react-router-hash-link.cjs.production";
+import { useState } from "react";
+import { Close, Menu } from "@mui/icons-material";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className="navbar">
-      <Link to="/" className="logo link">الراقى للتوصيل</Link>
-      <div className="links">
-        <NavLink to="/" className="link" activeclassname="active">الرئيسية</NavLink>
-        <HashLink className="link" smooth="true" to="/#choose-us">لماذا تختارنا</HashLink>
-        <HashLink className="link" smooth="true" to="/#car-types">انواع سيارتنا</HashLink>
-        <HashLink className="link" smooth="true" to="/#reviews">تقييمات العملاء</HashLink>
-        <HashLink className="link" smooth="true" to="/#faq">الاسئلة الشائعة</HashLink>
-        <HashLink className="link" smooth="true" to="/#contact-us">التواصل</HashLink>
+        <Link to="/" className="logo link large-screen ">الراقى للتوصيل</Link>
+        <div className="links large-screen">
+          <NavLink to="/" className="link" activeclassname="active">الرئيسية</NavLink>
+          <HashLink className="link" smooth="true" to="/#choose-us">لماذا تختارنا</HashLink>
+          <HashLink className="link" smooth="true" to="/#car-types">انواع سيارتنا</HashLink>
+          <HashLink className="link" smooth="true" to="/#reviews">تقييمات العملاء</HashLink>
+          <HashLink className="link" smooth="true" to="/#faq">الاسئلة الشائعة</HashLink>
+          <HashLink className="link" smooth="true" to="/#contact-us">التواصل</HashLink>
+        </div>
+      <Link to="/login" className="login link ">تسجيل الدخول</Link>
+      
+      <div className="smallscreen ">
+        <Menu className="overlay-open" sx={{fontSize:30}} onClick={ () => setToggleMenu(true) } />
+        { toggleMenu &&
+          <div  className="links overlay">
+            <Close className="overlay-close" sx={{fontSize:35}} onClick={ () => setToggleMenu(false) } />
+            <NavLink to="/" className="link" activeclassname="active" onClick={ () => setToggleMenu(false) }>الرئيسية</NavLink>
+            <HashLink className="link" smooth="true" to="/#choose-us" onClick={ () => setToggleMenu(false) }>لماذا تختارنا</HashLink>
+            <HashLink className="link" smooth="true" to="/#car-types" onClick={ () => setToggleMenu(false) }>انواع سيارتنا</HashLink>
+            <HashLink className="link" smooth="true" to="/#reviews" onClick={ () => setToggleMenu(false) }>تقييمات العملاء</HashLink>
+            <HashLink className="link" smooth="true" to="/#faq" onClick={ () => setToggleMenu(false) }>الاسئلة الشائعة</HashLink>
+            <HashLink className="link" smooth="true" to="/#contact-us" onClick={ () => setToggleMenu(false) }>التواصل</HashLink>
+          </div>
+        }
       </div>
-      <Link to="/login" className="login link">تسجيل الدخول</Link>
     </nav>
   )
-}
+};
 
 export default Navbar;
